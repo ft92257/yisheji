12aded7f49b1000b417c0207f19f7a93a7e0e79b
ft92257 <419302316@qq.com>
2013-12-23 16:55:17 +0800

    first6

--- Public/Library/qqAuth/comm/inc.php
+++ Public/Library/qqAuth/comm/inc.php
@@ -1,1 +1,1 @@
-{"appid":"100583832","appkey":"0749996bfa89612c4897560a7323ec21","callback":"http://yisheji.com/index.php?s=/User/qq_callback ","scope":"get_user_info","errorReport":true,"storageType":"file","host":"yisheji.com","user":"root","password":"root","database":"test"}
+{"appid":"100583832","appkey":"0749996bfa89612c4897560a7323ec21","callback":"http://yisheji.com/index.php?s=/User/qq_callback","scope":"get_user_info","errorReport":true,"storageType":"file","host":"yisheji.com","user":"root","password":"root","database":"test"}
