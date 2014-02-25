ea55c3b7d564483b1195a79206925477e4e558cb
douyingchu <617949577@qq.com>
2014-02-25 12:34:03 +0800

    dyc

--- Home/Index/Lib/Action/InitAction.class.php
+++ Home/Index/Lib/Action/InitAction.class.php
@@ -209,7 +209,8 @@ class InitAction extends BaseAction {
 	public function getData(){
 		$this->model = D($this->para['model']);
 		if(isset($this->para['where']))
-		$res = $this->model->where($this->para['where'])->select();
+			$this->model->where($this->para['where']);
+		$res = $this->model->select();
 		print_r('<pre>');
 		print_r($res);
 	}
